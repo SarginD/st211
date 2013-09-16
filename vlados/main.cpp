bb23b325532e7b19eea00cda5c127118e1dd6da2
vlad0s9 <vladinho.96@mail.ru>
2013-09-16 23:18:09 +0400

    Update

--- vlados/main.cpp
+++ vlados/main.cpp
@@ -1,9 +1,7 @@
-
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-
 
 int main(int argc, char *argv[])
 {
@@ -13,7 +11,7 @@ int main(int argc, char *argv[])
     int f1;
     
     if ((f1 = open(argv[1], O_RDWR, 0)) == -1)
-        perror("Не могу открыть файл");
+        perror("Не могу открыть файл\n");
     else
         printf("Файл открылся\n");  
     return 0;
