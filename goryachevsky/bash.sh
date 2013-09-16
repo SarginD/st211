eb73abd2d1ee4347a9a68b3ef4b448a2dc377dc7
student <student@ilab-vbox.(none)>
2013-09-16 23:00:06 +0400

    my bash

---
+++ goryachevsky/bash.sh
@@ -0,0 +1,25 @@
+ls
+echo njnjnds
+hostname
+env
+
+mkdir temp0
+cd temp0
+mkdir hometask
+cd hometask
+pwd
+
+touch 1.txt
+echo dvhnvjfv> 1.txt
+cd ../..
+
+ls
+touch 2.txt
+echo hcbdhbvhbd > 2.txt
+cp 2.txt temp0/hometask
+cd temp0/hometask
+rm 2.txt
+rm 1.txt
+cd ../..
+mv 2.txt temp0
+ls
