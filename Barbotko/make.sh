7a87d4231982f75eddadbcec94620341eb58619c
student <student@ilab-vbox.(none)>
2013-09-17 20:07:10 +0400

    Bash script is added

---
+++ Barbotko/make.sh
@@ -0,0 +1,19 @@
+ls
+mkdir folder
+cd folder
+touch foo.txt
+pwd
+echo im gonna print something > foo.txt
+cat foo.txt
+mv /home/student/hello.c ./
+gcc hello.c
+./a.out
+mkdir -p temp/temp2/temp3
+cd temp/temp2
+touch hi.txt
+cd temp3
+touch hello.txt
+cd ../../..
+pwd
+find . -name "*.txt"
+rm -r temp
