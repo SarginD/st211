c5e15c651a8697c4f746d2a36b427c6db785ca60
student <student@ilab-vbox.(none)>
2013-09-18 00:09:32 +0400

    syscalls reworked

--- Barbotko/sys_task.cpp
+++ Barbotko/sys_task.cpp
@@ -10,11 +10,13 @@ int main()
     int temp = open("stuff.cpp", O_RDONLY);
     if (temp == -1)
         perror("Oh my gosh! The file wasn't opened properly!");
+        
     printf( "User ID is %d\n", getuid() );
     printf( "Group ID is %d\n", getgid() );
-    if ( close(temp) == 0)
-        printf("The file was closed correctly\n");
-    else
-        printf("Something wrong with closing the file :C\n");
+    if (temp != -1)
+        if ( close(temp) == 0 )
+            printf("The file was closed correctly\n");
+        else
+            printf("Something wrong with closing the file :C\n");
     return 0;
 }
