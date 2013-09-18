b0be750f23d447f4ed7f94bcc01ec0d2a7c18490
Elroy <elroy@elroy-Aspire-V3-571G.(none)>
2013-09-18 10:29:20 +0400

    The closing of file added

--- Sobolev/sys_calls.c
+++ Sobolev/sys_calls.c
@@ -10,10 +10,16 @@ int main ( int argc, char *argv[] )
 	printf( "The user id is %d\n", getuid() );
 	printf( "The group id is %d\n",getgid() );
 	perror( "oops, somithing went wrong\n" );
-	int wow = open( argv[1], 0_TEXT );
+	int wow = open( argv[1], 0_RDONLY );
 	if (wow == -1)
-		perror( "oops, you can not open this text" );
+		perror( "oops, you can not open this file\n" );
 	else
-		printf( "succesfully opened" );
+	{
+		printf( "succesfully opened\n" );
+		if ( close(wow) == 0 )
+			printf("succesfully closed\n");
+		esle
+			perror( "oops, can not open this file" )
+	}
 	return 0;
 }
