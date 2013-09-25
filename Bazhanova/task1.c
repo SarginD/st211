cf1cb38fbea22e9f9b1a83f3ec8c4cf4d725015c
Ася <ashiya@ASHIYA-PC2.(none)>
2013-09-25 15:13:44 +0400

    problems were corrected

--- Bazhanova/task1.c
+++ Bazhanova/task1.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+#include <fcntl.h>
 
 int main ()
 {
 	int my_id = 0, group_id = 0;
 	int my_notes = 0, bad = 0;
-	my_notes = open ("note.txt", "O_RDWR");
-	bad = open ("note2.txt", "O_RDONLY");
+	my_notes = open ("note.txt", O_RDWR);
+	bad = open ("note2.txt", O_RDONLY);
 	my_id = getuid();
 	group_id = getgid();
 	printf ("my id is %d. \n group id is %d. \n", my_id, group_id);
 	if (my_notes != -1)
-		perror ("Can't open the file");
+		perror ("Error");
 	else
 		close (my_notes);
 	if (bad == -1)
-		perror ("The file is OK");
+		perror ("Error");
 	close(bad);
 	return 0;
 
