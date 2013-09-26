900eef8350f13e6ac7f7535455cc57c4a1f1bad0
SarginD <avterks@gmail.com>
2013-09-25 17:00:33 -0700

    Add working task2.c, but still with some bugs. Over tasks renamed

--- Sargin/task2.c
+++ Sargin/task2.c
@@ -1,15 +1,60 @@
-#include<stdio.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main()
-{
-	int a;
+int read_s(FILE *input, char *command, char *time ) {
+	char buf[50],pt;
+	fgets(buf, sizeof(buf), input);
+	int i = 0;
+	pt = buf[i];
+//	while ((pt == ' ') || (pt == '\t')) pt = buf[++i];
+//	if (pt == '\n')
+	while (pt != ';') {
+		*command++=pt;
+		pt = buf[++i];
+		}
+	*command = '\0';
+	pt=buf[++i];
+	while ((pt != '\n') && (pt != '\0') && (pt != ' ') && ( pt != EOF)) {
+		*time++=pt;
+		pt = buf[++i];
+		}
+	*time = '\0';
+					
+	return pt;
+	}
 
-	a=open("core.txt", O_WRONLY | O_CREAT | O_TRUNC , S_IREAD | S_IWRITE);
-	if ( a == -1)
-	perror("open failed on output file");
-	printf("User's ID is %ld\nReal Group's ID is %ld\n", getuid(), getgid());
+int main(int argc, char *argv[]){
+	if (argc == 1) {
+		perror("Write filename in the command line when you start the programm");
+		exit(1);
+                }
+	FILE *input = fopen(argv[1],"r");
+	char command[500], time_char[50];
+	double time_sec;
+	pid_t pid, pid_longest;
+	double time_longest = 0;
+	read_s(input, command, time_char);
+	do { 
+		sscanf(time_char, "%lf", &time_sec);
+		if ((pid = fork()) == 0) {
+			assert(usleep(1000*1000*time_sec) == 0);
+//			printf("Command =  %s    Time = %s\n", command, time_char);
+			execlp(command, command, (char*) NULL);
+			exit(1);
+			}
+		if (time_sec > time_longest) {
+			time_longest = time_sec;
+			pid_longest = pid;
+			}
+		read_s(input, command, time_char);
+		} while (!feof(input));
+	int status;
+	waitpid(pid_longest, &status, 0);
+	fclose(input);
 	return 0;
-}
+	}
