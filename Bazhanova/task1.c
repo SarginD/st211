3b948b43e02ac65f532d03158650f4b910166c8a
ashiya-hime <ashiya-hime@yandex.ru>
2013-09-26 17:38:34 +0400

    all problems were corrected, everything is OK

--- Bazhanova/task1.c
+++ Bazhanova/task1.c
@@ -6,19 +6,18 @@
 int main ()
 {
 	int my_id = 0, group_id = 0;
-	int my_notes = 0, bad = 0;
+	int my_notes = 0;
 	my_notes = open ("note.txt", O_RDWR);
-	bad = open ("note2.txt", O_RDONLY);
+	if (my_notes == -1)
+		perror ("Error: can't open the file");
 	my_id = getuid();
 	group_id = getgid();
 	printf ("my id is %d. \n group id is %d. \n", my_id, group_id);
-	if (my_notes != -1)
-		perror ("Error");
-	else
-		close (my_notes);
-	if (bad == -1)
-		perror ("Error");
-	close(bad);
+	if (my_notes !== -1)
+		if (close (my_notes == 0))
+			printf ("Closed correctly");
+		else 
+			printf ("Some problems with closing the file");
 	return 0;
 
 }
