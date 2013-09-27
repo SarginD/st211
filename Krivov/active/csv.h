5ec8060bf7739f21319de3b7da7d88f5e9f2d82d
Artemiy <avriy@me.com>
2013-09-27 18:21:52 +0400

    Bazhanova task1 8; Svorovsky task1 10; Parinov task1 10

--- Krivov/active/csv.h
+++ Krivov/active/csv.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int csv_read(FILE *file, char *statement, int maxlen); 
 /*Reads one statement from file, writes it to statement, returns ending symbol (EOF, '\n' or ';')
 Can also return '\0' if there are no more statements. In this case nothing was written to statement.*/
