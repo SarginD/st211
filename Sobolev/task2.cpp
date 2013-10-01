523b27ddbf003d36598a5b5da1c2a2834a589a8e
Elroy <elroy@elroy-Aspire-V3-571G.(none)>
2013-10-02 01:18:47 +0400

    Update almost working code, but there are some problems with ending programm. Please fix me.

--- Sobolev/task2.cpp
+++ Sobolev/task2.cpp
@@ -4,7 +4,8 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <unistd.h>
-//#include <wait.h>
+#include <wait.h>
+#include <string.h>
 
 struct process {
 	char com_text[20];
@@ -12,48 +13,72 @@ struct process {
 };
 
 
-process pread(FILE * f_in);
+process p_read(FILE * f_in);
 
-int main(int argc, char * argv[]){
+int main(int argc, char * argv[]) {
 	FILE * f_in = fopen(argv[1], "r");
-	process tmp = pread(f_in);
-	int pid;
-//  FIXME: parse went wrong! in a case "ls;4" tmp gets com_text value "ls/B"
+	process p_arr[20];
+	int i = 0;
 
-    
-	while(tmp.delay != -1){
-		pid = fork();
-		if (pid < 0)
-			perror("Process can not be created\n");
-		if (pid != 0)
-			waitpid(pid, NULL, 0);
-		else {
-			sleep(tmp.delay);
-			execlp(tmp.com_text, tmp.com_text, NULL);
-		}
-		tmp = pread(f_in);
+	p_arr[i] = p_read(f_in);
+	while (p_arr[i].delay != -1) {
+		i++;
+		p_arr[i] = p_read(f_in);
 	}
+
+	const int com_num = i;
+
+	for (int j = 0; j < com_num; j++) {
+		for (int i = 0; i < com_num - 1 - j; i++) {
+			if (p_arr[i].delay > p_arr[i + 1].delay) {
+				process swap = p_arr[i + 1];
+				p_arr[i + 1] = p_arr[i];
+				p_arr[i] = swap;
+			}
+		}
+	}
+
+	for (int i = 0; i < com_num; i++) {
+		printf("%s %d\n", p_arr[i].com_text, p_arr[i].delay);
+	}
+	int cur_delay = p_arr[0].delay;
+	for (int i = 0; i < com_num; i++) {
+		int pid = fork();
+//		printf("%s\n", p_arr[i].com_text);
+		if (pid == 0) {
+			usleep(cur_delay);
+			execlp(p_arr[i].com_text, p_arr[i].com_text, NULL);
+			int * status;
+			waitpid(p_arr[i].delay, status, 0);
+		}
+		if (i != com_num - 1)
+			cur_delay = p_arr[i + 1].delay - p_arr[i].delay;
+	}
+	fclose(f_in);
 	return 0;
 }
 
-process pread(FILE * f_in){
+process p_read(FILE * f_in) {
 	process cur;
-	char temp;
-	int time;
+	double time;
 	int i = 0;
-	process end = {"That's all", -1};
-    
-	while (1){
-		fscanf(f_in, "%c", &temp);
-		if(temp == ';')
+	char buf[10] = {};
+	process end = {"That is all", -1};
+
+	while (1) {
+		char temp = getc(f_in);
+		
+		if (temp == '\n')
+			temp = getc(f_in);
+		if (temp == ';')
 			break;
-		if(temp == EOF)
+		if (temp == EOF)
 			return end;
-		cur.com_text[i] = temp;
+		buf[i] = temp;
 		i++;
 	}
-	fscanf(f_in, "%d", &time);
+	strcpy(cur.com_text, buf);
+	fscanf(f_in, "%lf", &time);
 	cur.delay = 1000000*time;
-	fgetc(f_in);
 	return cur;
 }
