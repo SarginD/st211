2ff990ef2e40edb4088b0a62b5836fb576bfc2d0
Egor-Krivov <egor.krivov@frtk.ru>
2013-10-05 03:56:15 +0600

    Update recover_tools.h

--- backup_project/headers/recover_tools.h
+++ backup_project/headers/recover_tools.h
@@ -1,4 +1,4 @@
-int backup_folder(int revision, const char *path, const char *base);
+int recover_folder(int revision, const char *path, const char *base);
 //Backups folder from path to base with right revision. Returns 1 if at least one file or folder from the path was processed correctly otherwise returns 0.
 int recover_file(int revision, const char *path, const char *base);
 //Recovers file from base to psth with right revision. Returns 1 if file was processed correctly otherwise returns 0.
