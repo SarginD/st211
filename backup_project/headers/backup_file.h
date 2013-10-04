3f81f74949d42ea60af49ec2316131dce496d4e7
Egor Krivov <egor.krivov@frtk.ru>
2013-10-04 18:59:55 +0600

    backup headers were uploaded

---
+++ backup_project/headers/backup_file.h
@@ -0,0 +1,2 @@
+int backup_file(int revision, const char *path, const char *base);
+//Backups file from path to base with right revision. Returns 1 if file was processed correctly otherwise returns 0.
