6331de0c9e98f943d6a63859b3c538ea20876e75
Egor-Krivov <egor.krivov@frtk.ru>
2013-10-06 00:21:25 +0400

    Update backup_tools.h

--- backup_project/headers/backup_tools.h
+++ backup_project/headers/backup_tools.h
@@ -1,4 +1,4 @@
 int backup_folder(int revision, const char *path, const char *base);
-//Backups folder from path to base with right revision. Returns 1 if at least one file or folder from the path was processed correctly otherwise returns 0.
+//Backups folder from path to base with right revision. Returns 0 if at least one file or folder from the path was processed correctly otherwise returns 1.
 int backup_file(int revision, const char *path, const char *base);
-//Backups file from path to base with right revision. Returns 1 if file was processed correctly otherwise returns 0.
+//Backups file from path to base with right revision. Returns 0 if file was processed correctly otherwise returns 1.
