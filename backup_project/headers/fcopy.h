c9c4d68435b502b242811dc9890c635309c36fc6
SarginD <avterks@gmail.com>
2013-10-05 23:11:47 -0700

    Update backup_tools.h, add hash.h and fcopy.h

---
+++ backup_project/headers/fcopy.h
@@ -0,0 +1,2 @@
+int fcopy(const char *output, const char *input);
+// copy information from output file to the new input file
---
+++ backup_project/headers/hash.h
@@ -0,0 +1,2 @@
+long int hash(FILE *input);
+// hash function, returns long int hash value
